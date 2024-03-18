0981397c8980eace32eb158b8540657ad62e1dbb
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-19 00:18:25 +0700

    feat: register commands

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,7 @@
 require("dotenv").config();
 
 export const config = {
-  token: process.env.BOT_TOKEN,
+  clientId: process.env.BOT_CLIENT_ID ?? "",
+  guildId: process.env.BOT_GUILD_ID ?? "",
+  token: process.env.BOT_TOKEN ?? "",
 };

f8a81fbc36d8c2165c34626e2f1d02ea68dc1c11
Idhibhat Pankam <marginpankam@gmail.com>
2024-03-28 00:47:29 +0700

    feat: mention handler

---
+++ src/handlers/mention.ts
@@ -0,0 +1,27 @@
+import { Message } from "discord.js";
+import { askChatGPT3_5, translate } from "../chatgpt";
+import { noContentPrompt } from "../chatgpt/prompts";
+import { logger } from "../logger/logger";
+
+export const mentionHandler = async (message: Message) => {
+  logger.info(`mentionHandler: ${message.content}`);
+
+  if (
+    message.content.includes("@here") ||
+    message.content.includes("@everyone")
+  )
+    return false;
+
+  const rawContent = message.content;
+  const regex = /<@&\d+>/g;
+  const content = rawContent.replace(regex, "").trim();
+
+  if (content === "") {
+    const reply = await translate(noContentPrompt);
+    await message.channel.send(reply);
+    return;
+  }
+
+  const reply = await askChatGPT3_5(content);
+  await message.channel.send(reply);
+};
